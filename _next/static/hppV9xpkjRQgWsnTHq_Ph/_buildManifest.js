c575d697f59ab3dc91618d9154c91760264d454f
DooleyDigital <115346653+DooleyDigital@users.noreply.github.com>
2022-12-07 13:07:07 +0000

    Deploying to gh-pages from @ DooleyDigital/DDWeb@49e709a2bc3c2f7e3c6919728328f1f1282b7c6f 🚀

---
+++ _next/static/hppV9xpkjRQgWsnTHq_Ph/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-333a9d074fc6b187.js","static/chunks/963-f634484bda5be0d8.js","static/chunks/pages/index-347d58d33c34c43b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
